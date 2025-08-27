4b0b5f6a792d4c823a7474b789e135d487a2f3ac
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-27 22:19:55 +0500

    bajardim

--- if05.py
+++ if05.py
@@ -1,1 +1,11 @@
 
+c=float(input("Haroratni kiriting:"))
+
+if 0>c:
+    print("Juda sovuq! Issiq kiyim kiying.")
+
+if 0<=c<=14:
+    print("Sovuq.Kurktka kiying.")
+
+if 15<=c:
+    print("Ob-havo yaxshi.")
