aa57087b31c9641f5be3c269fbe032e137c279ac
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-27 22:00:57 +0500

    bajardim

--- if03.py
+++ if03.py
@@ -1,1 +1,13 @@
+a=float(input("Son kiriting:"))
 
+if  a%2==0:
+
+    print("Bu son 2 ga bo'linadi")
+
+if a%3==0:
+
+    print("Bu son 3 ga bo'linadi")
+    
+if a%5==0:
+
+    print("Bu son 5 ga bo'linadi")
