0362bdbc8f204a33c61abcfe5875f7e31d8b2a09
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-27 22:41:18 +0500

    bajardim

--- ifelse02.py
+++ ifelse02.py
@@ -1,1 +1,12 @@
+parol=input("Parol kiriting:")
+
+b=len(parol) ==8
+
+if b in ('A', 'z'):
+
+    print("Parol qabul qilindi")
+
+else:
+
+    print("Parol xato. Kamida 8 belgi, 1 harf va 1 raqm bo'lishi kerak")
 
