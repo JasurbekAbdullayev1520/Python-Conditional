bdb5a3ecc26269ada33e7e65ca597848f43ae955
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-27 22:12:41 +0500

    bajardim

--- if04.py
+++ if04.py
@@ -1,1 +1,15 @@
 
+a=int(input("Yoshingizni kiriting:"))
+
+if 0<=a<=6:
+
+    print("Chegirma 50%")
+
+if 7<=a<=17:
+    print("Chegirma 20%")
+
+if 18<=a<60:
+    print("Chegirma yo'q")
+
+if a>=60:
+    print("Chegirma 30%")
