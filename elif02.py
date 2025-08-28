0e8edf85309a24c4cd30b729ec83c5b5288fccfb
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-28 08:56:17 +0500

    bajardim

--- elif02.py
+++ elif02.py
@@ -1,1 +1,25 @@
 
+a=float(input("1-sonni kiriting:"))
+
+b=float(input("2-sonni kiriting"))
+
+c=input("amal:")
+
+if c=='-':
+    print(a-b)
+
+elif c=='+':
+    print(a+b)
+
+elif c=='*':
+    print(a*b)
+
+elif c=='/':
+    print(a/b)
+
+    if a==0 or b==0:
+        print( "0 ga bo'linmaydi")
+
+else:
+    print()
+
