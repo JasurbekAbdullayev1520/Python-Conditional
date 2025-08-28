0d4c1beb011afcc49b6184d8c4fa546b8800a77a
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-28 07:20:17 +0500

    bajardim

--- elif01.py
+++ elif01.py
@@ -1,1 +1,22 @@
 
+a=int(input("(0-100) gacha ball kiriting:"))
+
+if 90<=a<=100:
+    print("A (A'lo)")
+
+elif 80<=a<89:
+    print("B (Yaxshi)")
+
+elif 70<=a<79:
+    print("C (qoniqarli)")
+
+elif 60<=a<69:
+    print("D (Qoniqarli)")
+
+elif 0<=a<59:
+    print("F (Rad)")
+
+else:
+    print("Ball 0-100 oraig'ida bo'lishi kerak")
+
+   
