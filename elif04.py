2cc5634cfe644150744053357ebcf6321a11f754
JasurbekAbdullayev1520 <jasurbek.abdullayev1520@gmail.com>
2025-08-28 10:45:32 +0500

    bajardim

--- elif04.py
+++ elif04.py
@@ -1,1 +1,16 @@
 
+a=float(input("Masofani kiriting: "))
+
+if 0<=a<1:
+    print("Piyoda yuring: ")
+
+elif 1<=a<=5:
+    print("Velosiped yoki elektr skuter: ")
+
+elif 5<a<=50:
+    print("Avtobus yoki mashina: ")
+
+elif 50<a:
+    print("Poyezd yoki samalyot:")
+    if 0>a:
+        print("Masofa manfiy bo'la olmaydi.")
